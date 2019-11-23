2b74774d1659c7444b5815427c0edb89c604bc6b
Jorge Armando Ibarra Palacios <jorgeibarra87@gmail.com>
2019-11-23 14:54:24 -0500

    Proyecto inicial

---
+++ index.js
@@ -0,0 +1,7 @@
+//alert("hola Pltzi");
+$(function(){
+    //var header = document.getElementById('app-header');
+    //console.log('header');
+    var header = $('#app-header h1')
+    console.log(header)
+})
